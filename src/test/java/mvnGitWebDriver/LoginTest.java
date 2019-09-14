7bd423c666653ad20d55e9a0f5100aa3d567d4c3
Vishesh <saxena.vishesh2@gmail.com>
2019-09-14 16:53:02 +0530

    Initial Commit

---
+++ src/test/java/mvnGitWebDriver/LoginTest.java
@@ -0,0 +1,37 @@
+package mvnGitWebDriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest {
+
+	public static WebDriver driver;
+
+	@BeforeSuite
+	public void setUp() {
+		
+		WebDriverManager.chromedriver().setup();
+		driver= new ChromeDriver();
+	}
+	
+	@Test
+	public void doLogin() {
+		
+		driver.get("http://gmail.com");
+		driver.findElement(By.id("identifierId")).sendKeys("saxena.vishesh2@gmail.com");
+		//driver.findElement(By.id("Passwd")).sendKeys("ABCD");
+	}
+	
+	@AfterSuite
+	public void tearDown() {
+		
+		driver.close();
+	}
+	
+}
